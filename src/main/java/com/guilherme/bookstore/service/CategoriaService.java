0fc4f287f513d1c2042786bc5305261ac54ecb8c
GuiCartaxo <guilherme.cartaxo@hotmail.com>
2021-06-12 16:10:59 -0300

    findById para Categorias

---
+++ src/main/java/com/guilherme/bookstore/service/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.guilherme.bookstore.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guilherme.bookstore.domain.Categoria;
+import com.guilherme.bookstore.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	public Categoria findById(Integer id) {
+
+		Optional<Categoria> obj = categoriaRepository.findById(id);
+
+		return obj.orElse(null);
+	}
+}
