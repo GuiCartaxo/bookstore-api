6410dfdbf67edf6c76f135b1f1356bdef6cae7b4
GuiCartaxo <guilherme.cartaxo@hotmail.com>
2021-06-13 00:15:00 -0300

    Exceção personalizada para DELETE de categorias

--- src/main/java/com/guilherme/bookstore/service/CategoriaService.java
+++ src/main/java/com/guilherme/bookstore/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.guilherme.bookstore.domain.Categoria;
@@ -24,33 +25,39 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public List<Categoria> findAll() {
-		
+
 		return categoriaRepository.findAll();
 	}
-	
+
 	public Categoria create(Categoria obj) {
-		
+
 		obj.setId(null);
-		
+
 		return categoriaRepository.save(obj);
 	}
 
 	public Categoria update(Integer id, CategoriaDTO objDto) {
 
 		Categoria obj = findById(id);
-		
+
 		obj.setNome(objDto.getNome());
 		obj.setDescricao(objDto.getDescricao());
-		
+
 		return categoriaRepository.save(obj);
 	}
 
 	public void delete(Integer id) {
-		
+
 		findById(id);
-		
-		categoriaRepository.deleteById(id);
+
+		try {
+			categoriaRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new com.guilherme.bookstore.service.exceptions.DataIntegrityViolationException(
+					"Categoria não pode ser deletada! Possui livros associados");
+		}
+
 	}
 }
