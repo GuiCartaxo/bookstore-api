b8f2648f8986f243092fed498468da1de669b014
GuiCartaxo <guilherme.cartaxo@hotmail.com>
2021-06-12 23:39:20 -0300

    Criação do POST de categorias

--- src/main/java/com/guilherme/bookstore/service/CategoriaService.java
+++ src/main/java/com/guilherme/bookstore/service/CategoriaService.java
@@ -28,4 +28,11 @@ public class CategoriaService {
 		
 		return categoriaRepository.findAll();
 	}
+	
+	public Categoria create(Categoria obj) {
+		
+		obj.setId(null);
+		
+		return categoriaRepository.save(obj);
+	}
 }
